9d332015a10dc486109c75c3307bb75e4f6d9702
David Vetter <ddvetter23@gmail.com>
2022-10-31 22:23:57 -0500

    completed base

--- modules/fizzBuzz.js
+++ modules/fizzBuzz.js
@@ -1,10 +1,13 @@
+// Test Approved
 const fizzBuzz = (num) => {
-    if (num % 3 === 0) {
-        return 'Fizz';
+    if (num % 3 === 0 && num % 5 === 0) {
+        return 'FizzBuzz';
     } else if (num % 5 === 0) {
         return 'Buzz'
+    } else if (num % 3 === 0) {
+        return 'Fizz'
     } else {
-        return undefined;
+        return num;
     }
 }
 
--- tests/jest.test.js
+++ tests/jest.test.js
@@ -1,13 +1,39 @@
 const fizzBuzz = require('../modules/fizzBuzz');
-
+const leapYears = require('../modules/leapYears');
+// Pass
 test(' 6 is divisable by 3, should return "Fizz"', ()=>{
     expect(fizzBuzz(6)).toBe('Fizz');
 });
-
-test('4 is  not divisable by 3, should return "undefined"', ()=>{
-    expect(fizzBuzz(4)).toBe(undefined);
+// Pass
+test('4 is  not divisable by 3, should return 4', ()=>{
+    expect(fizzBuzz(4)).toBe(4);
 });
-
-test('10 is divisable by 5, should return "undefined"', ()=>{
+// Pass
+test('10 is divisable by 5, should return "Buzz"', ()=>{
     expect(fizzBuzz(10)).toBe('Buzz');
 });
+// Pass
+test('34 is  not divisable by 5, should return 34', ()=>{
+    expect(fizzBuzz(34)).toBe(34);
+});
+// Pass
+test('15 is divisable by 5 and 3, should return "FizzBuzz"', ()=>{
+    expect(fizzBuzz(15)).toBe('FizzBuzz');
+});
+
+// Function to determine if a year is a leap year
+test('1980 is a leap year, returns true', ()=>{
+    expect(leapYears(1980)).toBe(true);
+})
+
+test('1979 is NOT a leap year, returns false', ()=>{
+    expect(leapYears(1979)).toBe(false);
+})
+
+test('2000 is a leap year, returns true', ()=>{
+    expect(leapYears(2000)).toBe(true);
+})
+
+test('1900 is not a leap year, returns false', ()=>{
+    expect(leapYears(1900)).toBe(false);
+} )
